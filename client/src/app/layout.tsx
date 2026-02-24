a256099b56f6d3515ae0df5b1ddf84bfe026a6ca
Erzhan <code.ninja.erzhan@gmail.com>
2026-02-25 02:41:44 +0500

    Initial frontend part

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ReactNode } from "react";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["cyrillic"],
+});
+
+export const metadata: Metadata = {
+  title: "Academic",
+  description: "Platform for master students",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
